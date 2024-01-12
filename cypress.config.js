b861331741501017cfe3aa6d775cd93c40ac6674
vafaabadi <vafaabadi12@gmail.com>
2024-01-12 01:02:55 +0000

    Side Branch

--- cypress.config.js
+++ cypress.config.js
@@ -74,3 +74,4 @@ module.exports = defineConfig({
 */
  
 // check if "push" also triggers Actions on side branches or not. This is second commit. This is third commit.
+// check if workflow gets triggered on push to side branch
